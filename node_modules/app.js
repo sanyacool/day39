d545dc3b6f9e9a054323353d70ad7667d3a0d92f
Aleksandr <sanyacool2010@mail.ru>
2017-05-28 01:55:45 +0800

    myday42

---
+++ node_modules/app.js
@@ -0,0 +1,27 @@
+﻿var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/in', function(req, res){
+  res.sendFile(__dirname + '/in.html');
+});
+
+app.get('/out', function(req, res){
+  res.sendFile(__dirname + '/out.html');
+});
+ 
+io.on('connection', function(socket){
+  var name = 'U' + (socket.id).toString().substr(1,4);
+  socket.on('button clicked', function(val){
+    io.emit('button clicked', val);
+  });
+});
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
--- static/script.js
+++ static/script.js
@@ -6,13 +6,26 @@ var p = "px";
 //var canvas = document.getElementById("canvas");
 stage = new createjs.Stage("canvas");
 
-/*var gridOX = [
+//var gridOX = [-1, -1, -1, -1, -1, -1, -1, -1, -1];
+var gridOX = [
 	[-1, -1, -1],
 	[-1, -1, -1],
 	[-1, -1, -1]
-];*/
+];
+var figure = 0;
 
+var data = {
+	images: ["/static/xo.png"],
+	frames: {width:150, height:146},
+	animations: {
+		cross: 0,
+		circle: 1
+	}
+};
+var spriteSheet = new createjs.SpriteSheet(data);
+	
 console.log('hi!');
+// Рисуем сетку
 var grid = new createjs.Shape();
 grid.graphics.beginFill("black").drawRect(parseFloat(document.getElementById("canvas").width) / 3, 0, 2, parseFloat(document.getElementById("canvas").height));
 grid.graphics.beginFill("black").drawRect(parseFloat(document.getElementById("canvas").width) / 3 * 2, 0, 2, parseFloat(document.getElementById("canvas").height));
@@ -23,29 +36,48 @@ stage.update();
 
 stage.on("stagemousedown", CreateXO);
 
-function CreateXO(evt) {
+function CreateXO(evt) { //Рисуем крестики-нолики
 	
 	//console.log('hi!');
+	let column = Math.floor(evt.stageX / 300);
+	let line = Math.floor(evt.stageY / 300);
+	let X = 300 * column + 150;
+	let Y = 300 * line + 150;
+	//console.log('line = ' + line + ' column = ' + column);
 	
-	let X = 300 * Math.floor(evt.stageX / 300) + 150;
-	let Y = 300 * Math.floor(evt.stageY / 300) + 150;
-
-	console.log('X = ' + evt.stageX + ' Y = ' + evt.stageY);
-	//gridOX[X][Y] = 1;
-	var data = {
-		images: ["/static/xo.png"],
-		frames: {width:150, height:146},
-		animations: {
-			cross: 0,
-			circle: 1
+	
+	if (figure == 1) {
+		if (gridOX[line][column] == -1) {
+			console.log('Create Cross in X = ' + Math.floor(evt.stageX) + ' Y = ' + Math.floor(evt.stageY));
+			let cross = new createjs.Sprite(spriteSheet, "cross");
+			cross.regX = 75;
+			cross.regY = 75;
+			cross.x = X;
+			cross.y = Y;		
+			stage.addChild(cross);
+			stage.update();
+			figure = 0;
+			gridOX[line][column] = 0;
+		} else {
+				console.log('Figure already exists!');
+				alert('Figure already exists!');
 		}
-	};
-	var spriteSheet = new createjs.SpriteSheet(data);
-	var cross = new createjs.Sprite(spriteSheet, "cross");
-	cross.regX = 75;
-	cross.regY = 75;
-	cross.x = X;
-	cross.y = Y;		
-	stage.addChild(cross);
-	stage.update();
-}
+	} else {
+			if (gridOX[line][column] == -1) {
+				console.log('Create Circle in X = ' + Math.floor(evt.stageX) + ' Y = ' + Math.floor(evt.stageY));
+
+				let circle = new createjs.Sprite(spriteSheet, "circle");
+				circle.regX = 75;
+				circle.regY = 75;
+				circle.x = X;
+				circle.y = Y;		
+				stage.addChild(circle);
+				stage.update();
+				figure = 1;
+				gridOX[line][column] = 1;
+		} else {
+				console.log('Figure already exists!');
+				alert('Figure already exists!');
+		}
+	}
+};
