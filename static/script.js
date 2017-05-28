9252afa4ae97244c92a3a981ffaa908fcdd9ead9
Aleksandr <sanyacool2010@mail.ru>
2017-05-28 21:22:04 +0800

    myday 42

--- static/script.js
+++ static/script.js
@@ -5,15 +5,6 @@ var p = "px";
 
 //var canvas = document.getElementById("canvas");
 stage = new createjs.Stage("canvas");
-
-//var gridOX = [-1, -1, -1, -1, -1, -1, -1, -1, -1];
-var gridOX = [
-	[-1, -1, -1],
-	[-1, -1, -1],
-	[-1, -1, -1]
-];
-var figure = 0;
-
 var data = {
 	images: ["/static/xo.png"],
 	frames: {width:150, height:146},
@@ -23,8 +14,19 @@ var data = {
 	}
 };
 var spriteSheet = new createjs.SpriteSheet(data);
+stage.on("stagemousedown", CreateXO);
+
+//var gridOX = [-1, -1, -1, -1, -1, -1, -1, -1, -1];
+var gridOX = [
+	[-1, -1, -1],
+	[-1, -1, -1],
+	[-1, -1, -1]
+];
+
+var figure = 'circle';
+var count = 0;
 	
-console.log('hi!');
+console.log('New Game');
 // Рисуем сетку
 var grid = new createjs.Shape();
 grid.graphics.beginFill("black").drawRect(parseFloat(document.getElementById("canvas").width) / 3, 0, 2, parseFloat(document.getElementById("canvas").height));
@@ -34,8 +36,6 @@ grid.graphics.beginFill("black").drawRect(0, parseFloat(document.getElementById(
 stage.addChild(grid);
 stage.update();
 
-stage.on("stagemousedown", CreateXO);
-
 function CreateXO(evt) { //Рисуем крестики-нолики
 	
 	//console.log('hi!');
@@ -44,40 +44,108 @@ function CreateXO(evt) { //Рисуем крестики-нолики
 	let X = 300 * column + 150;
 	let Y = 300 * line + 150;
 	//console.log('line = ' + line + ' column = ' + column);
-	
-	
-	if (figure == 1) {
-		if (gridOX[line][column] == -1) {
-			console.log('Create Cross in X = ' + Math.floor(evt.stageX) + ' Y = ' + Math.floor(evt.stageY));
-			let cross = new createjs.Sprite(spriteSheet, "cross");
-			cross.regX = 75;
-			cross.regY = 75;
-			cross.x = X;
-			cross.y = Y;		
-			stage.addChild(cross);
-			stage.update();
-			figure = 0;
+	let kas = gridOX[line][column];
+	if (gridOX[line][column] == -1) {
+		console.log('Create ' + figure + ' in X = ' + Math.floor(evt.stageX) + ' Y = ' + Math.floor(evt.stageY));
+		let fig = new createjs.Sprite(spriteSheet, figure);
+		fig.regX = 75;
+		fig.regY = 75;
+		fig.x = X;
+		fig.y = Y;		
+		stage.addChild(fig);
+		stage.update();
+		//Проверяем ничью
+		count++;
+		if (count == 9) {
+			alert('Draw!');
+			Restart();
+			return
+		}
+		
+		if (figure == 'cross') {
+			gridOX[line][column] = 1;
+			checkVictory();
+			figure = 'circle';
+		} else {
 			gridOX[line][column] = 0;
-		} else {
-				console.log('Figure already exists!');
-				alert('Figure already exists!');
-		}
+			checkVictory();
+			figure = 'cross';
+		};
+		
 	} else {
-			if (gridOX[line][column] == -1) {
-				console.log('Create Circle in X = ' + Math.floor(evt.stageX) + ' Y = ' + Math.floor(evt.stageY));
-
-				let circle = new createjs.Sprite(spriteSheet, "circle");
-				circle.regX = 75;
-				circle.regY = 75;
-				circle.x = X;
-				circle.y = Y;		
-				stage.addChild(circle);
-				stage.update();
-				figure = 1;
-				gridOX[line][column] = 1;
-		} else {
-				console.log('Figure already exists!');
-				alert('Figure already exists!');
-		}
-	}
+		console.log('Figure already exists!');
+		alert('Figure already exists!');
+	};
+	
 };
+
+function checkVictory() {
+	// Желательно оптимизировать
+	// If Circles Wins
+	if (gridOX[0][0] == 0 && gridOX[1][1] == 0 && gridOX[2][2] == 0)
+		BlueWin();
+	if (gridOX[0][2] == 0 && gridOX[1][1] == 0 && gridOX[2][0] == 0)
+		BlueWin();
+	if (gridOX[0][0] == 0 && gridOX[0][1] == 0 && gridOX[0][2] == 0)
+		BlueWin();
+	if (gridOX[1][0] == 0 && gridOX[1][1] == 0 && gridOX[1][2] == 0)
+		BlueWin();
+	if (gridOX[2][0] == 0 && gridOX[2][1] == 0 && gridOX[2][2] == 0)
+		BlueWin();
+	if (gridOX[0][0] == 0 && gridOX[1][0] == 0 && gridOX[2][0] == 0)
+		BlueWin();
+	if (gridOX[0][1] == 0 && gridOX[1][1] == 0 && gridOX[2][1] == 0)
+		BlueWin();
+	if (gridOX[0][2] == 0 && gridOX[1][2] == 0 && gridOX[2][2] == 0)
+		BlueWin();
+	// If Crosses Wins
+	if (gridOX[0][0] == 1 && gridOX[1][1] == 1 && gridOX[2][2] == 1)
+		RedWin();
+	if (gridOX[0][2] == 1 && gridOX[1][1] == 1 && gridOX[2][0] == 1)
+		RedWin();
+	if (gridOX[0][0] == 1 && gridOX[0][1] == 1 && gridOX[0][2] == 1)
+		RedWin();
+	if (gridOX[1][0] == 1 && gridOX[1][1] == 1 && gridOX[1][2] == 1)
+		RedWin();
+	if (gridOX[2][0] == 1 && gridOX[2][1] == 1 && gridOX[2][2] == 1)
+		RedWin();
+	if (gridOX[0][0] == 1 && gridOX[1][0] == 1 && gridOX[2][0] == 1)
+		RedWin();
+	if (gridOX[0][1] == 1 && gridOX[1][1] == 1 && gridOX[2][1] == 1)
+		RedWin();
+	if (gridOX[0][2] == 1 && gridOX[1][2] == 1 && gridOX[2][2] == 1)
+		RedWin();
+};
+
+function BlueWin() {
+	alert('Circles Won!!');
+	Restart();
+}
+
+function RedWin() {
+	alert('Crosses Won!!');
+	Restart();
+	
+}
+function Restart() {
+	// Не получилось сослаться на начало кода, чтобы не повторять все то, что было уже вначале
+	// тем создания функции вроде init. Программа просто игнорировала функцию
+	stage.removeAllChildren();
+	gridOX = [
+		[-1, -1, -1],
+		[-1, -1, -1],
+		[-1, -1, -1]
+	];
+	count = 0;
+	console.log('New Game');
+	// Рисуем сетку
+	// Можно сетку реализовать в <div>, чтобы ее каждый раз не перерисовывать
+	var grid = new createjs.Shape();
+	grid.graphics.beginFill("black").drawRect(parseFloat(document.getElementById("canvas").width) / 3, 0, 2, parseFloat(document.getElementById("canvas").height));
+	grid.graphics.beginFill("black").drawRect(parseFloat(document.getElementById("canvas").width) / 3 * 2, 0, 2, parseFloat(document.getElementById("canvas").height));
+	grid.graphics.beginFill("black").drawRect(0, parseFloat(document.getElementById("canvas").height) / 3, parseFloat(document.getElementById("canvas").width), 2);
+	grid.graphics.beginFill("black").drawRect(0, parseFloat(document.getElementById("canvas").height) / 3 * 2, parseFloat(document.getElementById("canvas").width), 2);
+	stage.addChild(grid);
+	stage.update();
+	// Неплохо было бы реализовать счет
+}
